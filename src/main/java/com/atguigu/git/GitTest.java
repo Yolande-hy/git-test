fef965c9471a9d048e48e41d7b750cdda8582299
Yonande <1392861988@qq.com>
2022-09-13 09:33:49 +0800

    913change

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,11 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello.git!");
-        System.out.println("hello.git!");
-        System.out.println("master test");
-        System.out.println("hello.git!");
-        System.out.println("hot-fix test!");
-
-
+        System.out.println("913change");
     }
 }
