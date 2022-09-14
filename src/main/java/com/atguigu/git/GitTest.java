bd73aeb8363e4f65989d38ffe564e1bac8c329fb
Yolande-hy <110910905+Yolande-hy@users.noreply.github.com>
2022-09-14 17:10:22 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello.git!");
         System.out.println("913change");
+        System.out.println("test");
     }
 }
